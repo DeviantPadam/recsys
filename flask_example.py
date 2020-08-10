1e295d3fc0a746a8e0e09388bb44bbf008e4ca99
Padam Gupta <padamgupta1999@gmail.com>
2020-08-11 00:27:09 +0530

    Add files via upload

---
+++ flask_example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 23:55:25 2020
+
+@author: deviantpadam
+"""
+
+from flask import Flask
+from torch2vec.torch2vec import LoadModel
+
+app = Flask(__name__)
+model = LoadModel('/home/deviantpadam/weights.npy')
+
+@app.route('/')
+def main():
+    
+    ids, prob = model.similar_docs('7JTc5wMQU4XyEGmtFhQTYq',topk=10,use='torch')
+
+    ids = "------------".join(ids)
+    type(ids)
+    
+    prob= [str(i) for i in prob]
+    prob = "-----------".join(prob)        
+    return ids+prob
+
+if '__main__'==__name__:
+    app.run()

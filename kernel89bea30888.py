22b309ae8bdde619a5bb1828b360170ce01c54ee
Padam Gupta <padamgupta1999@gmail.com>
2020-08-11 13:09:16 +0530

    Update kernel89bea30888.py

--- kernel89bea30888.py
+++ kernel89bea30888.py
@@ -15,7 +15,7 @@ data.vocab_builder()
 
 doc, context, target_noise_ids = data.get_data(window_size=3,num_noise_words=6)
 
-model = DM(vec_dim=100,num_docs=len(data),num_words=data.vocab_size)
+model = DM(vec_dim=100,num_docs=len(data),num_words=data.vocab_size).cuda()
 
 num_workers = os.cpu_count()
 model.fit(doc_ids=doc,context=context,target_noise_ids=target_noise_ids,epochs=5,batch_size=3000,num_workers=num_workers)
